ecfd63e915679e25a02c9ec47a01386e324798ca
James Moak <mrjmoak3@gmail.com>
2017-11-19 17:53:56 -0800

    implemented qstack

--- src/Chapter3.java
+++ src/Chapter3.java
@@ -34,6 +34,26 @@ public class Chapter3 {
         }
     }
 
+    static void runMinStack() {
+        System.out.println("Min Stack");
+        MinStack s = new MinStack();
+        s.push(3);
+        s.push(5);
+        System.out.println("Stack: 5,3");
+        System.out.println("Min: " + s.getMin());
+        s.push(2);
+        s.push(1);
+        System.out.println("Stack: 1,2,5,3");
+        System.out.println("Min: " + s.getMin());
+        s.pop();
+        System.out.println("Stack: 2,5,3");
+        System.out.println("Min: " + s.getMin());
+        s.pop();
+        System.out.println("Stack: 5,3");
+        System.out.println("Min: " + s.getMin());
+        System.out.println();
+    }
+
     static class StackStack {
         Stack<Stack<Integer>> s = new Stack<Stack<Integer>>();
         int size=0;
@@ -67,26 +87,6 @@ public class Chapter3 {
         }
     }
 
-    static void runMinStack() {
-        System.out.println("Min Stack");
-        MinStack s = new MinStack();
-        s.push(3);
-        s.push(5);
-        System.out.println("Stack: 5,3");
-        System.out.println("Min: " + s.getMin());
-        s.push(2);
-        s.push(1);
-        System.out.println("Stack: 1,2,5,3");
-        System.out.println("Min: " + s.getMin());
-        s.pop();
-        System.out.println("Stack: 2,5,3");
-        System.out.println("Min: " + s.getMin());
-        s.pop();
-        System.out.println("Stack: 5,3");
-        System.out.println("Min: " + s.getMin());
-        System.out.println();
-    }
-
     static void runStackStack() {
         System.out.println("Stack Stack");
         StackStack s = new StackStack();
@@ -110,8 +110,57 @@ public class Chapter3 {
         System.out.println("numStacks: " + s.numStacks);
     }
 
+    static class QStack {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+        int size=0;
+
+        public int size() {return size;}
+        public int push(int n) {
+            ++size;
+            return s1.push(n);
+        }
+        public int pop() {
+            if (s2.size() > 0) {
+                return s2.pop();
+            }
+            else {
+                while (s1.size() > 0) {
+                    s2.push(s1.pop());
+                }
+                return s2.pop();
+            }
+        }
+    }
+
+    static void runQStack() {
+        System.out.println("");
+        System.out.println("Q Stack");
+        QStack s = new QStack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        System.out.println("Popping: " + s.pop() + " expecting 1");
+        s.push(7);
+        System.out.println("Popping: " + s.pop() + " expecting 2");
+        System.out.println("Popping: " + s.pop() + " expecting 3");
+
+        System.out.println("Popping: " + s.pop() + " expecting 4");
+        System.out.println("Popping: " + s.pop() + " expecting 5");
+        s.push(8);
+        System.out.println("Popping: " + s.pop() + " expecting 6");
+        s.push(9);
+        System.out.println("Popping: " + s.pop() + " expecting 7");
+        System.out.println("Popping: " + s.pop() + " expecting 8");
+        System.out.println("Popping: " + s.pop() + " expecting 9");
+    }
+
     public static void run() {
         runMinStack();
         runStackStack();
+        runQStack();
     }
 }
